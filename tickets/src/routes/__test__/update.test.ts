29eb580215874844ad910e3a9ba00426145f71e9
Oluwaseun Akogun <seun407@gmail.com>
2021-05-08 15:54:52 +0800

    initial commit

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,105 @@
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+
+it('returns a 404 if the user provided id does not exist', async () => {
+  const id = mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'jkkfkjd',
+      price: 20,
+    })
+    .expect(404);
+});
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: 'jkkfkjd',
+      price: 20,
+    })
+    .expect(401);
+});
+
+it('returns a 401 if the user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'kksjsjs',
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'djhfkwi',
+      price: 38,
+    })
+    .expect(401);
+});
+
+it('returns a 400 if user provides an invalid title or price', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'kksjsjs',
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'jdjjkdiuw',
+      price: -10,
+    })
+    .expect(400);
+});
+
+it('updates the  ticket, provided valid inputs', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'kksjsjs',
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100,
+    })
+    .expect(200);
+
+  const ticketReponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketReponse.body.title).toEqual('new title');
+  expect(ticketReponse.body.price).toEqual(100);
+});
