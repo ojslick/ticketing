e96bb1464846eb07c4927b433e7eaddb691d0231
Oluwaseun Akogun <seun407@gmail.com>
2021-05-08 22:47:06 +0800

    created order service and fixed nats-connect bug

---
+++ orders/src/models/order.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
