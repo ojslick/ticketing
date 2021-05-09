f37989a2f88fb0a3222c58d3402bf6fdad082b3c
Oluwaseun Akogun <seun407@gmail.com>
2021-05-09 23:30:44 +0800

    created the order and ticket models in orders service

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose';
+import { OrderStatus } from '@ojembatickets/common';
+import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
@@ -17,3 +19,44 @@ interface OrderDoc extends mongoose.Document {
 interface OrderModel extends mongoose.Model<OrderDoc> {
   build(attrs: OrderAttrs): OrderDoc;
 }
+
+const orderSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expireAt: {
+      types: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>(
+  'Order',
+  orderSchema
+);
+
+export { Order };
